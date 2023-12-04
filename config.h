3aac6922a6773ee80ada48fa094a7f5ebb7e02da
René Wirnata <rene.wirnata@pandascience.net>
2023-12-04 15:05:50 +0100

    Remove deprecated mod-tap config
    IGNORE_MOD_TAP_INTERRUPT is no longer necessary as it is now the
    default behavior of mod-tap keys.

--- config.h
+++ config.h
@@ -31,7 +31,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // ---------- TAP vs HOLD SETTINGS --------------------------------------------
 #define TAPPING_TERM 200
-#define IGNORE_MOD_TAP_INTERRUPT
 #undef PERMISSIVE_HOLD
 #undef PREVENT_STUCK_MODIFIERS
 
